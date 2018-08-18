bb2f01da37e8bcccccd60d6faee68c579569980c
Pipeline Author <nfjsuser1@gmail.com>
2018-08-17 21:29:19 -0400

    update for testing

---
+++ src/test/java/TestExample2.java
@@ -0,0 +1,12 @@
+// File: src/test/java/TestExample2.java
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestExample2 {
+
+  @Test public void example2() {
+
+	Assert.assertEquals("Gradle testing is easy", "Gradle testing is easy");
+  }
+}
